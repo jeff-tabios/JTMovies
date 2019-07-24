25f7f9ce205201c3ef527493938520faab93005a
jeff-tabios <jefftabios1@gmail.com>
2019-07-24 16:44:10 +0800

    Added Models

---
+++ JTMovies/Models/Movies.swift
@@ -0,0 +1,51 @@
+//
+//  Movies.swift
+//  JTMovies
+//
+//  Created by Jeff Tabios on 24/07/2019.
+//  Copyright © 2019 Jeff Tabios. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Movies
+struct Movies: Codable {
+    let page, totalResults, totalPages: Int
+    let results: [Result]
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+        case results
+    }
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let voteCount, id: Int
+    let video: Bool
+    let voteAverage: Double
+    let title: String
+    let popularity: Double
+    let posterPath: String?
+    let originalLanguage, originalTitle: String
+    let genreIDS: [Int]
+    let backdropPath: String?
+    let adult: Bool
+    let overview, releaseDate: String
+    
+    enum CodingKeys: String, CodingKey {
+        case voteCount = "vote_count"
+        case id, video
+        case voteAverage = "vote_average"
+        case title, popularity
+        case posterPath = "poster_path"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case genreIDS = "genre_ids"
+        case backdropPath = "backdrop_path"
+        case adult, overview
+        case releaseDate = "release_date"
+    }
+}
