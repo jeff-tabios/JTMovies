bf6744b4f74eb2cf5c066edb324838c92502258d
jeff-tabios <jefftabios1@gmail.com>
2019-07-25 11:31:43 +0800

    Updated project files and added constants and placeholder image

---
+++ JTMovies/Application/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  JTMovies
+//
+//  Created by Jeff Tabios on 25/07/2019.
+//  Copyright © 2019 Jeff Tabios. All rights reserved.
+//
+
+import Foundation
+
+struct AppConstants{
+    static let imageBaseUrl = "https://image.tmdb.org/t/p/w185_and_h278_bestv2"
+}
