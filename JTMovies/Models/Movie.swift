102a23daac7bbb2d4b4106c193d4074d81ee9885
jeff-tabios <jefftabios1@gmail.com>
2019-07-25 17:27:33 +0800

    Refactored movie model

--- JTMovies/Models/Movie.swift
+++ JTMovies/Models/Movie.swift
@@ -10,56 +10,21 @@ import Foundation
 
 // MARK: - Movie
 struct Movie: Codable {
-    let adult: Bool
-    let backdropPath: String
-    let belongsToCollection: BelongsToCollection
-    let budget: Int
+   
     let genres: [Genre]
-    let homepage: String
     let id: Int
-    let imdbID, originalLanguage, originalTitle, overview: String
-    let popularity: Double
-    let posterPath: String
-    let productionCompanies: [ProductionCompany]
-    let productionCountries: [ProductionCountry]
-    let releaseDate: String
-    let revenue, runtime: Int
+    let overview: String
+    let posterPath: String?
     let spokenLanguages: [SpokenLanguage]
-    let status, tagline, title: String
-    let video: Bool
-    let voteAverage: Double
-    let voteCount: Int
+    let title: String
     
     enum CodingKeys: String, CodingKey {
-        case adult
-        case backdropPath = "backdrop_path"
-        case belongsToCollection = "belongs_to_collection"
-        case budget, genres, homepage, id
-        case imdbID = "imdb_id"
-        case originalLanguage = "original_language"
-        case originalTitle = "original_title"
-        case overview, popularity
+        
+        case genres, id
+        case overview
         case posterPath = "poster_path"
-        case productionCompanies = "production_companies"
-        case productionCountries = "production_countries"
-        case releaseDate = "release_date"
-        case revenue, runtime
         case spokenLanguages = "spoken_languages"
-        case status, tagline, title, video
-        case voteAverage = "vote_average"
-        case voteCount = "vote_count"
-    }
-}
-
-// MARK: - BelongsToCollection
-struct BelongsToCollection: Codable {
-    let id: Int
-    let name, posterPath, backdropPath: String
-    
-    enum CodingKeys: String, CodingKey {
-        case id, name
-        case posterPath = "poster_path"
-        case backdropPath = "backdrop_path"
+        case title
     }
 }
 
@@ -67,29 +32,6 @@ struct BelongsToCollection: Codable {
 struct Genre: Codable {
     let id: Int
     let name: String
-}
-
-// MARK: - ProductionCompany
-struct ProductionCompany: Codable {
-    let id: Int
-    let logoPath, name, originCountry: String
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case logoPath = "logo_path"
-        case name
-        case originCountry = "origin_country"
-    }
-}
-
-// MARK: - ProductionCountry
-struct ProductionCountry: Codable {
-    let iso3166_1, name: String
-    
-    enum CodingKeys: String, CodingKey {
-        case iso3166_1 = "iso_3166_1"
-        case name
-    }
 }
 
 // MARK: - SpokenLanguage
