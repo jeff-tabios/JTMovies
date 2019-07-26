9cb0089324b511fdda367d26a26751ea574b3ad6
jeff-tabios <jefftabios1@gmail.com>
2019-07-26 19:27:40 +0800

    Added book now view

---
+++ JTMovies/Views/BookNowViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BookNowViewController.swift
+//  JTMovies
+//
+//  Created by Jeff Tabios on 26/07/2019.
+//  Copyright © 2019 Jeff Tabios. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class BookNowViewController: UIViewController {
+
+    @IBOutlet weak var wb: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let url = URL(string: "https://www.cathaycineplexes.com.sg/")!
+        wb.load(URLRequest(url: url))
+        wb.allowsBackForwardNavigationGestures = true
+    }
+    
+}
